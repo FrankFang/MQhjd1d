b15f0ad961ce4c59c139bd0fe32acf12d4f30f48
frankfang <frankfang1990@gmail.com>
2022-03-12 23:19:02 +0800

    创建model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show'
 end
