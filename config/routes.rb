008e0a33c34a5b5197838eee14ef409bff9a2b58
frankfang <frankfang1990@gmail.com>
2022-03-22 22:53:32 +0800

    添加根路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/', to: 'home#index'
 
   namespace :api do
     namespace :v1 do 
