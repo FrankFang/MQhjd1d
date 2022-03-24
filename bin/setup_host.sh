4e07d8b7c897816ba78c3f6f12bc8e7c0312e710
frankfang <frankfang1990@gmail.com>
2022-03-24 22:53:42 +0800

    成功部署到宿主机

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -10,5 +10,5 @@ if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
   docker rm -f $container_name
 fi
 echo 'docker run ...'
-docker run -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
+docker run -e DB_HOST=$DB_HOST -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
 echo 'DONE!'
