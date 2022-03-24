753184be340b5c29685554ecbeebdce88a9ac18d
frankfang <frankfang1990@gmail.com>
2022-03-24 14:32:04 +0800

    完善setup_host.sh

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -5,8 +5,10 @@ version=$(cat mangosteen_deploy/version)
 
 echo 'docker build ...'
 docker build mangosteen_deploy -t mangosteen:$version
+if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
+  echo 'docker rm ...'
+  docker rm -f $container_name
+fi
 echo 'docker run ...'
 docker run -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
-echo 'docker exec ...'
-docker exec -it $container_name bin/rails db:create db:migrate
 echo 'DONE!'
