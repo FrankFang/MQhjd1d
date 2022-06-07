269a472df77c69e7ef56e0850b76b9a025137ae6
frankfang <frankfang1990@gmail.com>
2022-06-07 23:17:55 +0800

    完成 /api/v1/items 筛选功能

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.page(params[:page])
+    items = Item.where({created_at: params[:created_after]..params[:created_before]})
+      .page(params[:page])
     render json: { resources: items, pager: {
       page: params[:page],
       per_page: 100,
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
-  describe "index by page" do
-    it "works! (now write some real specs)" do
+  describe "获取账目" do
+    it "分页" do
       11.times { Item.create amount: 100 }
       expect(Item.count).to eq 11
       get '/api/v1/items'
@@ -13,6 +13,17 @@ RSpec.describe "Items", type: :request do
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 1
+    end
+    it "按时间筛选" do
+      item1 = Item.create amount: 100, created_at: Time.new(2018, 1, 2)
+      item2 = Item.create amount: 100, created_at: Time.new(2018, 1, 2)
+      item3 = Item.create amount: 100, created_at: Time.new(2019, 1, 1)
+      get '/api/v1/items?created_after=2018-01-01&created_before=2018-01-03'
+      expect(response).to have_http_status 200
+      json = JSON.parse(response.body)
+      expect(json['resources'].size).to eq 2
+      expect(json['resources'][0]['id']).to eq item1.id
+      expect(json['resources'][1]['id']).to eq item2.id
     end
   end
   describe "create" do
