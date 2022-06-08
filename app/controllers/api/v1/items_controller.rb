2b98fdccea3f6d8d387a9e1c08f57d7c506b8703
frankfang <frankfang1990@gmail.com>
2022-06-08 22:09:20 +0800

    修复文档的小问题

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::ItemsController < ApplicationController
     items = Item.where({created_at: params[:created_after]..params[:created_before]})
       .page(params[:page])
     render json: { resources: items, pager: {
-      page: params[:page],
-      per_page: 100,
+      page: params[:page] || 1,
+      per_page: Item.default_per_page,
       count: Item.count
     }}
   end
