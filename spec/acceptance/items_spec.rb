3ef12b8aa66a78b97caffdbd10de2b9fb0afe21f
frankfang <frankfang1990@gmail.com>
2022-06-09 00:28:31 +0800

    重构 API 文档

--- spec/acceptance/items_spec.rb
+++ spec/acceptance/items_spec.rb
@@ -3,6 +3,7 @@ require 'rspec_api_documentation/dsl'
 
 resource "账目" do
   get "/api/v1/items" do
+    authentication :basic, :auth
     parameter :page, '页码'
     parameter :created_after, '创建时间起点（筛选条件）' 
     parameter :created_before, '创建时间终点（筛选条件）' 
@@ -12,17 +13,10 @@ resource "账目" do
     end
     let(:created_after) { '2020-10-10'}
     let(:created_before) { '2020-11-11'}
+    let(:current_user) { User.create email: '1@qq.com' }
+    let(:auth) { "Bearer #{current_user.generate_jwt}" }
     example "获取账目" do
-      user1 = User.create email: '1@qq.com'
-      11.times do Item.create amount: 100, created_at: '2020-10-30', user_id: user1.id end
-
-      jwt = ''
-      no_doc do 
-        client.post '/api/v1/session', email: user1.email, code: '123456'
-        json = JSON.parse response_body
-        jwt = json['jwt']
-      end
-      header 'Authorization', "Bearer #{jwt}"
+      11.times do Item.create amount: 100, created_at: '2020-10-30', user_id: current_user.id end
       do_request
       expect(status).to eq 200
       json = JSON.parse response_body
