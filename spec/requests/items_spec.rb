4f40cdd15f3dccba6ea59f7ac1daba405408bc09
frankfang <frankfang1990@gmail.com>
2022-06-08 21:25:05 +0800

    添加边界情况

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -15,15 +15,23 @@ RSpec.describe "Items", type: :request do
       expect(json['resources'].size).to eq 1
     end
     it "按时间筛选" do
-      item1 = Item.create amount: 100, created_at: Time.new(2018, 1, 2)
-      item2 = Item.create amount: 100, created_at: Time.new(2018, 1, 2)
-      item3 = Item.create amount: 100, created_at: Time.new(2019, 1, 1)
+      item1 = Item.create amount: 100, created_at: '2018-01-02'
+      item2 = Item.create amount: 100, created_at: '2018-01-02'
+      item3 = Item.create amount: 100, created_at: '2019-01-01'
       get '/api/v1/items?created_after=2018-01-01&created_before=2018-01-03'
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 2
       expect(json['resources'][0]['id']).to eq item1.id
       expect(json['resources'][1]['id']).to eq item2.id
+    end
+    it "按时间筛选（边界条件）" do
+      item1 = Item.create amount: 100, created_at: '2018-01-01'
+      get '/api/v1/items?created_after=2018-01-01&created_before=2018-01-02'
+      expect(response).to have_http_status 200
+      json = JSON.parse(response.body)
+      expect(json['resources'].size).to eq 1
+      expect(json['resources'][0]['id']).to eq item1.id
     end
   end
   describe "create" do
