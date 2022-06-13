40ebcc7dbec0cd96d1cc09fc96460a7e950bf9d6
frankfang <frankfang1990@gmail.com>
2022-06-13 22:07:02 +0800

    完善 API 文档

---
+++ spec/acceptance/sessions_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource "会话" do
+  post "/api/v1/session" do
+    parameter :email, '邮箱', required: true
+    parameter :code, '验证码', required: true
+    response_field :jwt, '用于验证用户身份的 token'
+    let(:email) { '1@qq.com' }
+    let(:code) { '123456' }
+    example "登录" do
+      User.create email: email
+      do_request
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      expect(json['jwt']).to be_a String
+    end
+  end
+end
