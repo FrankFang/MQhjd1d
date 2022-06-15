1476cc175f7b6c86fc60e2dbf200aaa71b84bb67
frankfang <frankfang1990@gmail.com>
2022-06-15 23:31:34 +0800

    更新 API 文档

--- spec/acceptance/items_spec.rb
+++ spec/acceptance/items_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "账目" do
+  let(:current_user) { User.create email: '1@qq.com' }
+  let(:auth) { "Bearer #{current_user.generate_jwt}" }
   get "/api/v1/items" do
     authentication :basic, :auth
     parameter :page, '页码'
@@ -11,16 +13,45 @@ resource "账目" do
       response_field :id, 'ID'
       response_field :amount, "金额（单位：分）"
     end
-    let(:created_after) { '2020-10-10'}
-    let(:created_before) { '2020-11-11'}
-    let(:current_user) { User.create email: '1@qq.com' }
-    let(:auth) { "Bearer #{current_user.generate_jwt}" }
+    let(:created_after) { Time.now - 10.days }
+    let(:created_before) { Time.now + 10.days }
     example "获取账目" do
-      11.times do Item.create amount: 100, created_at: '2020-10-30', user_id: current_user.id end
+      tag = Tag.create name: 'x', sign:'x', user_id: current_user.id
+      11.times do 
+        Item.create! amount: 100, happen_at: '2020-10-30', tags_id: [tag.id], 
+          user_id: current_user.id 
+      end
       do_request
       expect(status).to eq 200
       json = JSON.parse response_body
       expect(json['resources'].size).to eq 10
+    end
+  end
+
+  post "/api/v1/items" do
+    authentication :basic, :auth
+    parameter :amount, '金额（单位：分）', required: true
+    parameter :kind, '类型', required: true, enum: ['expenses', 'income']
+    parameter :happen_at, '发生时间', required: true
+    parameter :tags_id, '标签列表（只传ID）', required: true
+    with_options :scope => :resource do
+      response_field :id
+      response_field :amount
+      response_field :kind
+      response_field :happen_at
+      response_field :tags_id
+    end
+    let(:amount) { 9900 }
+    let(:kind) { 'expenses' }
+    let(:happen_at) { '2020-10-30T00:00:00+08:00' }
+    let(:tags) { (0..1).map{Tag.create name: 'x', sign:'x', user_id: current_user.id} }
+    let(:tags_id) { tags.map(&:id) }
+    let(:happen_at) { '2020-10-30T00:00:00+08:00' }
+    example "创建账目" do
+      do_request
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      expect(json['resource']['amount']).to eq amount
     end
   end
 end
