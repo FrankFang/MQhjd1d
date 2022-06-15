a6777f1f086142d9a8432fe8317a3b4538f943b2
frankfang <frankfang1990@gmail.com>
2022-06-15 21:57:52 +0800

    更新 API 文档

--- spec/acceptance/tags_spec.rb
+++ spec/acceptance/tags_spec.rb
@@ -5,6 +5,23 @@ resource "标签" do
   authentication :basic, :auth
   let(:current_user) { User.create email: '1@qq.com' }
   let(:auth) { "Bearer #{current_user.generate_jwt}" }
+  get "/api/v1/tags/:id" do
+    let (:tag) { Tag.create name: 'x', sign:'x', user_id: current_user.id }
+    let (:id) { tag.id }
+    with_options :scope => :resource do
+      response_field :id, 'ID'
+      response_field :name, "名称"
+      response_field :sign, "符号"
+      response_field :user_id, "用户ID"
+      response_field :deleted_at, "删除时间"
+    end
+    example "获取标签" do
+      do_request
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      expect(json['resource']['id']).to eq tag.id
+    end
+  end
   get "/api/v1/tags" do
     parameter :page, '页码'
     with_options :scope => :resources do
@@ -14,7 +31,7 @@ resource "标签" do
       response_field :user_id, "用户ID"
       response_field :deleted_at, "删除时间"
     end
-    example "获取标签" do
+    example "获取标签列表" do
       11.times do Tag.create name: 'x', sign:'x', user_id: current_user.id end
       do_request
       expect(status).to eq 200
