1a60038793bcda0f9c59a01a87120dc9b0604806
frankfang <frankfang1990@gmail.com>
2022-06-24 00:18:35 +0800

    修复 typo

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     if Rails.env.test?
       return render status: :unauthorized if params[:code] != '123456'
     else
-      canSignin = ValidationCodes.exists? email: params[:email], code: params[:code], used_at: nil
+      canSignin = ValidationCode.exists? email: params[:email], code: params[:code], used_at: nil
       return render status: :unauthorized unless canSignin
     end
     user = User.find_or_create_by email: params[:email]
