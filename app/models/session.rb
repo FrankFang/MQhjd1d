2566f12e55d4171d8155833480edcb23148a7e3e
frankfang <frankfang1990@gmail.com>
2022-06-28 22:02:31 +0800

    登录时做校验

---
+++ app/models/session.rb
@@ -0,0 +1,15 @@
+class Session
+  include ActiveModel::Model
+  attr_accessor :email, :code
+  validates :email, :code, presence: true
+  validates :email, format: {with: /\A.+@.+\z/}
+
+  validate :check_validation_code
+
+  def check_validation_code
+    return if Rails.env.test? and self.code == '123456'
+    return if self.code.empty?
+    self.errors.add :email, :not_found unless 
+      ValidationCode.exists? email: self.email, code: self.code, used_at: nil
+  end
+end
