29382b8f8d9af277b3c69a81d1d6dc999da38855
frankfang <frankfang1990@gmail.com>
2022-07-15 01:26:01 +0800

    验证码放到邮件标题里

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(email)
     validation_code = ValidationCode.order(created_at: :desc).find_by_email(email)
     @code = validation_code.code
-    mail(to: email, subject: '山竹记账验证码')
+    mail(to: email, subject: "[#{@code}]山竹记账验证码")
   end
 end
