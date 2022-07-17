37d0335994aed4deb1eb5478ba6c913d94b9b2ad
frankfang <frankfang1990@gmail.com>
2022-07-17 22:42:30 +0800

    Item amount 为 0 时不能提交

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   paginates_per 25
   enum kind: { expenses: 1, income: 2 }
   validates :amount, presence: true
+  validates :amount, numericality: { other_than: 0 }
   validates :kind, presence: true
   validates :happen_at, presence: true
   validates :tag_ids, presence: true
