33e5bee032b01ffaec0c4236302cbb612e0338eb
frankfang <frankfang1990@gmail.com>
2022-07-25 22:59:38 +0800

    配置 CORS

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem "rack-cors"
+gem "rack-cors"
 
 gem 'kaminari'
 gem 'rspec_api_documentation', path: './vendor/rspec_api_documentation'
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,13 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '121.196.236.94:8080'
+    resource '*',
+        methods: [:get, :post, :delete, :patch, :options, :head],
+        headers: :any,
+        expose: ['*', 'Authorization'],
+        max_age: 600
+  end
+end
