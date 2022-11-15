493225e87a8e4ef4c43cad2b8a0a85596b74adf6
frankfang <frankfang1990@gmail.com>
2022-11-15 22:07:51 +0800

    将路径 / 加入 jwt 白名单

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ gem 'jwt'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "debug", '~> 1.6.2', platforms: %i[ mri mingw x64_mingw ]
   gem 'rspec-rails', '~> 5.0.0'
 end
 
--- lib/auto_jwt.rb
+++ lib/auto_jwt.rb
@@ -4,15 +4,15 @@ class AutoJwt
   end
   def call(env)
     # jwt 跳过以下路径
-    return @app.call(env) if ['/api/v1/session','/api/v1/validation_codes'].include? env['PATH_INFO']
+    return @app.call(env) if ['/', '/api/v1/session', '/api/v1/validation_codes'].include? env['PATH_INFO']
 
     header = env['HTTP_AUTHORIZATION']
     jwt = header.split(' ')[1] rescue ''
     begin
-      payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' } 
+      payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' }
     rescue JWT::ExpiredSignature
       return [401, {}, [JSON.generate({reason: 'token expired'})]]
-    rescue  
+    rescue
       return [401, {}, [JSON.generate({reason: 'token invalid'})]]
     end
     env['current_user_id'] = payload[0]['user_id'] rescue nil
