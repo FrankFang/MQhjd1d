25b10ac942b8a56ad180561e3d374c111e1f379d
frankfang <frankfang1990@gmail.com>
2022-11-23 10:47:20 +0800

    在生产环境也允许使用 123456 作为验证码

--- app/models/session.rb
+++ app/models/session.rb
@@ -7,10 +7,10 @@ class Session
   validate :check_validation_code
 
   def check_validation_code
-    return if Rails.env.test? and self.code == '123456'
     return if self.code.empty?
-    self.errors.add :email, :not_found unless 
+    return if self.code == '123456'
+    self.errors.add :email, :not_found unless
       ValidationCode.exists? email: self.email, code: self.code, used_at: nil
   end
-  
+
 end
