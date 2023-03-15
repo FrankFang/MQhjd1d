5089a0f1c0b08b4381fb5caae2a0863980542d37
FrankFang <frankfang1990@gmail.com>
2023-03-15 17:27:04 +0800

    修改 Tag 的默认排序，按照 created_at 从大到小排序

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,12 +1,10 @@
 class Tag < ApplicationRecord
   paginates_per 25
-  enum kind: {expenses: 1, income: 2 }
+  default_scope { where(deleted_at: nil).order(created_at: :desc) }
+  enum kind: { expenses: 1, income: 2 }
   validates :name, presence: true
   validates :name, length: { maximum: 4 }
   validates :sign, presence: true
   validates :kind, presence: true
   belongs_to :user
-  def self.default_scope
-    where(deleted_at: nil)
-  end
 end
